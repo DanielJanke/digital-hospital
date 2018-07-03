2a1981e9998716ee437d605100edb4fb1ca14f38
daniel.janke <daniel.janke@apertomove.de>
2018-07-03 10:26:22 +0200

    WIP: Fix android version

--- components/AppointmentCard.js
+++ components/AppointmentCard.js
@@ -1,13 +1,7 @@
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity
-} from 'react-native';
-import { ProgressCircle } from 'react-native-svg-charts';
-import extStyles from '../assets/styles';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { ProgressCircle } from "react-native-svg-charts";
+import extStyles from "../assets/styles";
 
 type Props = {
   date: string,
@@ -30,36 +24,18 @@ export default class AppointmentCard extends Component<Props> {
   }
 
   render() {
-    const {
-      date,
-      flagColor,
-      title,
-      description,
-      time,
-      onPress,
-      progress,
-      firstEntry,
-      lastEntry
-    } = this.props;
+    const { date, flagColor, title, description, time, onPress, progress, firstEntry, lastEntry } = this.props;
     return (
       <View style={styles.container}>
         <View style={styles.dateContainer}>
-          <View
-            style={[styles.topLine, firstEntry ? {} : styles.lineVisible]}
-          />
+          <View style={[styles.topLine, firstEntry ? {} : styles.lineVisible]} />
           {date == undefined ? (
             <View style={styles.dot} />
           ) : (
-            <Text
-              style={[styles.date, date == 'JETZT' ? { color: '#316597' } : {}]}
-            >
-              {date}
-            </Text>
+            <Text style={[styles.date, date == "JETZT" ? { color: "#316597" } : {}]}>{date}</Text>
           )}
 
-          <View
-            style={[styles.bottomLine, lastEntry ? {} : styles.lineVisible]}
-          />
+          <View style={[styles.bottomLine, lastEntry ? {} : styles.lineVisible]} />
         </View>
         <TouchableOpacity onPress={onPress} style={styles.card}>
           <View style={[styles.flag, { backgroundColor: flagColor }]} />
@@ -75,16 +51,12 @@ export default class AppointmentCard extends Component<Props> {
                   strokeWidth={7}
                   animate={true}
                   progress={progress}
-                  progressColor={
-                    this.props.progress < 1
-                      ? 'rgba(49,155,255, 0.75)'
-                      : '#63F2C0'
-                  }
-                  backgroundColor={'rgba(0,50,97,0.15)'}
+                  progressColor={this.props.progress < 1 ? "rgba(49,155,255, 0.75)" : "#63F2C0"}
+                  backgroundColor={"rgba(0,50,97,0.15)"}
                 />
               ) : (
                 <Text style={extStyles.text.title}>{time}</Text>
-              )}{' '}
+              )}
               }
             </View>
           </View>
@@ -96,36 +68,36 @@ export default class AppointmentCard extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    alignSelf: 'stretch',
-    backgroundColor: '#F0F3F6',
-    flexDirection: 'row',
+    alignSelf: "stretch",
+    backgroundColor: "#F0F3F6",
+    flexDirection: "row",
     // flex: 1,
     paddingRight: 16,
 
-    alignItems: 'stretch'
+    alignItems: "stretch"
   },
   dateContainer: {
     width: 82,
-    alignSelf: 'stretch',
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignSelf: "stretch",
+    alignItems: "center",
+    justifyContent: "center"
   },
   topLine: {
     flex: 1,
     width: 2
   },
   lineVisible: {
-    backgroundColor: '#CACACA'
+    backgroundColor: "#CACACA"
   },
   bottomLine: {
     flex: 1,
     width: 2
   },
   date: {
-    textAlign: 'center',
+    textAlign: "center",
     marginVertical: 16,
-    fontWeight: 'bold',
-    color: '#737373'
+    fontWeight: "bold",
+    color: "#737373"
   },
   dot: {
     width: 12,
@@ -133,49 +105,49 @@ const styles = StyleSheet.create({
     marginVertical: 16,
     borderRadius: 12 / 2,
     // backgroundColor: "#4D4D4D",
-    backgroundColor: '#CACACA'
+    backgroundColor: "#CACACA"
   },
   card: {
-    backgroundColor: '#fff',
-    flexDirection: 'row',
+    backgroundColor: "#fff",
+    flexDirection: "row",
     flex: 1,
     marginVertical: 8,
     borderRadius: 4,
-    shadowColor: '#CBCBCB',
+    shadowColor: "#CBCBCB",
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.5,
     shadowRadius: 4
   },
   flag: {
-    backgroundColor: '#B2EB55',
+    backgroundColor: "#B2EB55",
     // width: 54,
     width: 18,
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
     borderBottomLeftRadius: 4,
     borderTopLeftRadius: 4
   },
   contentContainer: {
     paddingVertical: 16,
     paddingHorizontal: 16,
-    flexDirection: 'row',
+    flexDirection: "row",
     flex: 1,
-    justifyContent: 'space-between'
+    justifyContent: "space-between"
   },
   titleContainer: {},
   timeContainer: {
-    justifyContent: 'center'
+    justifyContent: "center"
   },
   time: {
-    fontWeight: 'bold'
+    fontWeight: "bold"
   },
   title: {
-    color: '#4E4E4E',
-    fontWeight: 'bold',
+    color: "#4E4E4E",
+    fontWeight: "bold",
     marginBottom: 4,
     fontSize: 17
   },
   description: {
-    color: '#737373',
+    color: "#737373",
     fontSize: 17,
     maxWidth: 270
   }
